42f5abde300f94cda6801adafdbc5988f023d365
dolphindad <90588017+dolphindad@users.noreply.github.com>
2022-08-09 17:42:26 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,218 @@
+let i = 1
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . .
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . . #
+        . # # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . . #
+        # # # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        # # . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        # # # . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # . . # #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # . . # #
+        # . . # #
+        # # # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # . . # #
+        # . # # #
+        # # # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # . . # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # . # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(i)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
